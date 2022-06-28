a3d69cb1cc47911a7817a4717d80e46f1cbc4b7a
unknown <matet2501@gmail.com>
2022-06-28 13:20:29 +1000

    add in second test to test reponse post code

--- src/test/java/com/api/test/TestAPISuite.java
+++ src/test/java/com/api/test/TestAPISuite.java
@@ -3,6 +3,7 @@ package com.api.test;
 import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class TestAPISuite {
@@ -15,6 +16,17 @@ public class TestAPISuite {
                 .get("https://digitalapi.auspost.com.au/postcode/search.json")
         .then()
                 .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void Verify_Response_Postcode4075(){
+        given().header("auth-key","8b8942e7-bb90-443b-b737-e108887c80de")
+                .param("q","oxley")
+                .param("state", "QLD")
+        .when()
+                .get("https://digitalapi.auspost.com.au/postcode/search.json")
+        .then()
+                .assertThat().body("localities.locality.postcode", equalTo(4075));
     }
 
 }
